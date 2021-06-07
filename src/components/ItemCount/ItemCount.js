813440f333a449c46c292874a6facb73fe07e350
Juan Pablo Mascaretti <jpmascaretti@uwaterloo.ca>
2021-06-07 08:27:42 -0500

    feedback added including prettifying and improved logic

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,24 +1,52 @@
-import React, {useState} from 'react'
-import './ItemCount.css'
+import React, { useState } from "react";
+import "./ItemCount.css";
 
-const ItemCount = ({stock, initial, onAdd}) => {
-    const[count,setCount] = useState(initial);
+const ItemCount = ({ stock, initial, onAdd }) => {
+  const [count, setCount] = useState(initial);
 
-    //Need to ask about onAdd as a callback. What do they mean by that? 
-    //Need to make the counter and the button responsive
+  //onAdd is just a placeholder callback fx
+  return (
+    <>
+      <div className="button__wrapper">
+        <button
+          className={
+            count > 0 && count <= stock
+              ? "button__count"
+              : "button__count--disabled"
+          }
+          disabled={count < initial}
+          onClick={() => count > 0 && setCount(count - 1)}
+        >
+          -
+        </button>
+        <h1>{count}</h1>
+        <button
+          className={
+            count < stock && count >= 0
+              ? "button__count"
+              : "button__count--disabled"
+          }
+          disabled={count >= stock}
+          onClick={() => count < stock && setCount(count + 1)}
+        >
+          +
+        </button>
+      </div>
+      <div className="button__wrapper--second">
+        <button
+          className={
+            count > 0 && count <= stock
+              ? "button__add-to-cart"
+              : "button__add-to-cart--disabled"
+          }
+          disabled={count > 0 && count <= stock ? false : true}
+          onClick={onAdd}
+        >
+          Add to Cart
+        </button>
+      </div>
+    </>
+  );
+};
 
-    return (
-        <>  
-            <div className="button__wrapper">
-            <button className={count > 0 && count <= stock ? "button__count": "button__count--disabled"} disabled={count > 0 && count <= stock ? false : true}onClick={() => count > 0 ? setCount(count-1) : setCount(count)}>-</button>
-            <h1>{count}</h1>
-            <button className={count < stock && count >= 0 ? "button__count": "button__count--disabled"} disabled={count < stock && count >= 0 ? false : true} onClick={() => count < stock ? setCount(count+1) : setCount(count)}  >+</button>
-            </div>
-            <div className="button__wrapper--second">
-            <button className={count > 0 && count <= stock ? "button__add-to-cart" : "button__add-to-cart--disabled"} disabled={count > 0 && count <= stock ? false : true} onClick={onAdd} >Add to Cart</button>
-            </div>
-        </>
-    )
-}
-
-export default ItemCount
+export default ItemCount;
