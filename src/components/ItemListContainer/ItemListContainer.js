f28876c85d20e4daf599a616336ba4b8ec4b26c9
Juan Pablo Mascaretti <jpmascaretti@uwaterloo.ca>
2021-06-19 14:25:43 -0500

    working on gif

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,41 +1,30 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import ItemList from "./ItemList/ItemList";
-import "./ItemListContainer.css";
-import {nftItems} from '../../Products/products';
+import { nftItems } from "../../Products/products";
 import { useParams } from "react-router-dom";
 
-
-const getProducts = (() => {
+const getProducts = () => {
   const itemPromise = new Promise((resolve, reject) => {
-    
-    setTimeout(resolve(nftItems), 2000)
-  }
-  );
-  return itemPromise
-  })
+    setTimeout(resolve(nftItems), 2000);
+  });
+  return itemPromise;
+};
 
 const ItemListContainer = (props) => {
-
   const [itemsFromPromise, setData] = useState([]);
-  const {categoryId} = useParams()
+  const { categoryId } = useParams();
 
   useEffect(() => {
-    getProducts().then(product => {
+    getProducts().then((product) => {
       if (categoryId === undefined) {
-        setData(product)
+        setData(product);
       } else {
-        setData(product.filter(nftItem => nftItem.category === categoryId))
+        setData(product.filter((nftItem) => nftItem.category === categoryId));
       }
-
-  } )
+    });
   }, [categoryId]);
-    
-  return (
-    
-    <ItemList items={itemsFromPromise}/>
-  );
 
-
+  return <ItemList items={itemsFromPromise} />;
 };
 
 export default ItemListContainer;
