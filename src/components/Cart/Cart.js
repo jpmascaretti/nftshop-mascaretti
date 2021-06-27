85fe53368004fd6d2617d9c0d83d7f4c1112abe6
Juan Pablo Mascaretti <jpmascaretti@uwaterloo.ca>
2021-06-27 18:41:04 -0500

    need to continue working on cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,22 +1,27 @@
-import React, {useContext} from 'react'
-import { ModeContext } from '../CartContext/CartContext'
-import './Cart.css'
+import React, { useContext } from "react";
+import { ModeContext } from "../CartContext/CartContext";
+import "./Cart.css";
 
 const Cart = () => {
-    const {cartState} = useContext(ModeContext)
+  const { cartState } = useContext(ModeContext);
 
-    return (
+  return (
+    <div>
+      {cartState.map((element, i) => 
+        typeof element === "object" ? (
         <ul>
-        {cartState.forEach(element => {
-            
-        })((element, i) => {
-            if (typeof(element)==='object') {
-                return <li key={i}>Item: </li>
-            }
-            // <li key={i} properties={element} />
-        })}
+          <li key={i}>{element.title} </li>
+          <li>Price: {element.price}.000 ETH</li>
+          <li>Quantity: {cartState[cartState.indexOf(element) + 1]}</li>
+          <li>Total: {element.price * cartState[cartState.indexOf(element) + 1]}.000 ETH</li>
         </ul>
-    )
-}
+        ) : null
+      )}
+      <div>
+        
+      </div>
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
