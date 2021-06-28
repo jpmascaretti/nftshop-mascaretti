0ce530616ae408d02b59bf62c367fd39f504f161
Juan Pablo Mascaretti <jpmascaretti@uwaterloo.ca>
2021-06-27 22:41:27 -0500

    just need to add styling to cart

--- src/components/NavigationBar/CartWidget/CartWidget.js
+++ src/components/NavigationBar/CartWidget/CartWidget.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Cart3 } from "react-bootstrap-icons";
+import { ModeContext } from "../../CartContext/CartContext";
 import "./CartWidget.css";
 
 function CartWidget() {
+  const { cartState } = useContext(ModeContext);
+  const [cartTotalQuantity, setCartTotalQuantity] = useState(0);
+
+  useEffect(() => {
+    let cartQuantity = 0;
+
+    cartState.forEach((cartItem) => {
+      if (typeof cartItem !== "object") {
+        cartQuantity = cartQuantity + cartItem;
+      }
+      setCartTotalQuantity(cartQuantity );
+    });
+  }, [cartState]);
+
+  //Note: need to add styling when number > 9 - easy conditional rendering with css class
   return (
-    <>
-      <a href="/cart">
-        <Cart3 id="cart__style" />
-        <span id="dot">
-          <p id="dot_number">2</p>
-        </span>
-      </a>
-    </>
+    <a href="/cart">
+      <Cart3 id="cart__style" />
+      <span id="dot">
+        <p id="dot_number">{cartTotalQuantity}</p>
+      </span>
+    </a>
   );
 }
 
