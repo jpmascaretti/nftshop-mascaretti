7fe95334faedb6eea87debcb01ac88c8367dafb8
Juan Pablo Mascaretti <jpmascaretti@uwaterloo.ca>
2021-07-05 17:26:28 -0500

    firebase filtering and spinner added

---
+++ src/components/CategorySpinner/CategorySpinner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Spinner} from 'react-bootstrap'
+import './CategorySpinner.css'
+
+const CategorySpinner = () => {
+  return (
+    <div className="spinner__loading">
+      <Spinner animation="border" role="status" variant="primary">
+        <span className="sr-only spinner__loading">Loading...</span>
+      </Spinner>
+    </div>
+  );
+};
+
+export default CategorySpinner;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,37 +1,68 @@
 import React, { useEffect, useState } from "react";
 import ItemList from "../ItemList/ItemList";
 import { useParams, Redirect } from "react-router-dom";
-import { db } from '../../firebase/firebase'
-
+import { db } from "../../firebase/firebase";
+import CategorySpinner from "../CategorySpinner/CategorySpinner"
 
 const ItemListContainer = (props) => {
   const [products, setProducts] = useState([]);
   const { categoryId } = useParams();
-
+  const [loading, setLoading] = useState(true);
+  
   useEffect(() => {
-
     const itemCollection = db.collection("nftproducts");
-    itemCollection.get().then((productQuery) => {
-      productQuery.docs.map(doc => ({id: doc.id, ...doc.data()}))
-      if (categoryId === undefined) {
-        setProducts(productQuery.docs.map(doc => ({id: doc.id, ...doc.data()})));
-      } else {
-        setProducts(productQuery.docs.map(doc => ({id: doc.id, ...doc.data()})).filter((nftItem) => nftItem.category === categoryId));
-      }
-    })
-    console.log(typeof(categoryId))
+    setLoading(true);
+
+    if (categoryId === undefined) {
+      itemCollection.get().then((productQuery) => {
+        setProducts(
+          productQuery.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+        );
+      }).then(() => setLoading(false))
+    } else if (categoryId === "common") {
+      const commonItems = itemCollection.where("category", "==", "common");
+      commonItems.get().then((productQuery) => {
+        setProducts(
+          productQuery.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+        );
+      }).then(() => setLoading(false))
+    } else if (categoryId === "rare") {
+      const rareItems = itemCollection.where("category", "==", "rare");
+      rareItems.get().then((productQuery) => {
+        setProducts(
+          productQuery.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+        );
+      }).then(() => setLoading(false))
+    } else if (categoryId === "epic") {
+      const epicItems = itemCollection.where("category", "==", "epic");
+      epicItems.get().then((productQuery) => {
+        setProducts(
+          productQuery.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+        );
+      }).then(() => setLoading(false))
+    } else if (categoryId === "legendary") {
+      const legendaryItems = itemCollection.where("category", "==", "legendary");
+      legendaryItems.get().then((productQuery) => {
+        setProducts(
+          productQuery.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+        );
+      }).then(() => setLoading(false))
+    }
   }, [categoryId]);
 
-
   if (categoryId === undefined) {
-    return <ItemList items={products} />;
-  } else if (categoryId === "common" || categoryId === "rare" || categoryId === "epic" || categoryId === "legendary" ) {
-    return <ItemList items={products} />;
+    return loading ? <CategorySpinner/> : <ItemList items={products} />;
+  } else if (
+    categoryId === "common" ||
+    categoryId === "rare" ||
+    categoryId === "epic" ||
+    categoryId === "legendary"
+  ) {
+    return loading === true ? <CategorySpinner/> : <ItemList items={products} />;
+
   } else {
-    return <Redirect to="/"/>
+    return <Redirect to="/" />;
   }
-
-
 };
 
 export default ItemListContainer;
